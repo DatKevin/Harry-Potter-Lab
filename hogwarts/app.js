f5ef909fd15b90379d0d98e59401ffa7d2461dcb
Kevin Dat Nguyen <datkevin@gmail.com>
2019-10-31 14:28:31 -0700

    Added list elements

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -24,3 +24,26 @@ container.append(h4)
 let wand = document.createElement("h4")
 wand.innerText = "Birch Wand with Pheonix Feather Core"
 container.append(wand)
+
+let storage = document.createElement("ul")
+storage.setAttribute("storage", "trunk")
+container.append(storage)
+
+for (let i = 1; i <= 6; i++) {
+	let listitem = document.createElement("li")
+	listitem.innerText = ""
+	storage.append(listitem)
+}
+
+document.querySelectorAll("li")[0].innerText = "Butterbeer"
+document.querySelectorAll("li")[1].innerText = "Invisibility Cloak"
+document.querySelectorAll("li")[2].innerText = "Magic Map"
+document.querySelectorAll("li")[3].innerText = "Time Turner"
+document.querySelectorAll("li")[4].innerText = "Leash"
+document.querySelectorAll("li")[5].innerText = "Bertie Bott's Every Flavor [Jelly] Beans."
+
+for (let i = 1; i <= 3; i++) {
+	document.querySelectorAll("li")[i].className = "secret"
+}
+
+document.querySelectorAll("li")[4].className = "badger"
